118c7bc00388934d1aac07eb3a844e7ac6a5538b
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2022-06-12 10:30:38 +0530

    Update p_ttishow.py

--- Pg/p_ttishow.py
+++ Pg/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('Updates', url='https://t.me/Groupdc')
+            InlineKeyboardButton('Updates', url='https://t.me/Murat_30_God')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
